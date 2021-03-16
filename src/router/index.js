f1d1d748c0076544be0cf5a20f074061f0682701
shanghaobo <1186156343@qq.com>
2021-03-16 16:48:19 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/demo',
+      path: '/',
       name: 'Demo',
       component: ()=>import('@/components/Demo.vue')
     }
